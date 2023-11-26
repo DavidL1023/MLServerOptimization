f6d3c7162b188f59e5eac36391993ed8905838f8
angiedmclean <58499172+angiedmclean@users.noreply.github.com>
2023-11-26 15:00:53 -0500

    Update Plots_For_ML.py

--- Plots_For_ML.py
+++ Plots_For_ML.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("exportforuconn.csv")
+chunksize = 100000
+
+tfr = pd.read_csv("exportforuconn.csv", chunksize=chunksize, iterator=True)
+df = pd.concat(tfr, ignore_index=True)
 
 data = df.columns
 
 df['_time'] = pd.to_datetime(df['_time']).astype(int) // 10**9
 df = df.fillna(0)
-df = df.iloc[0:1000, :]
 
-# Grouping by 'servers' column and creating separate DataFrames
 server_groups = df.groupby('host')
 
-# Creating separate DataFrames for each server
 separate_dataframes = {server: group for server, group in server_groups}
 
 print("Options for values are:")
@@ -35,13 +35,11 @@ for start in range(0, 286, 20):
             ax = axs[i]
             server = separate_dataframes[df]
             ax.plot(server['_time'], server[value], color='red', )
-            title_font = {'family': 'serif', 'color': 'blue', 'weight': 'bold', 'size': 6}
-            ax.set_title(df +': ' + value + ' Over Time', fontdict=title_font)
+            font = {'size': 6}
+            ax.set_title(df +': ' + value + ' Over Time', fontdict=font)
 
     plt.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.95, wspace=0.3, hspace=0.5)
 
-# Adjusting layout
 plt.tight_layout()
 
-# Show plot
 plt.show()
