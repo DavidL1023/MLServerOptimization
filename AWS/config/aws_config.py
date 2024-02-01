2f01f3115e19a4e53e399162fcc7144c6a557605
David <dev.davidlacava@gmail.com>
2024-02-01 01:53:17 -0500

    added env file, fixed path error

--- AWS/config/aws_config.py
+++ AWS/config/aws_config.py
@@ -1,8 +1,23 @@
 import boto3
 import json
+import os
+from dotenv import load_dotenv
+
+# Be sure to have a .env file with
+"""
+access_key_id=your_key
+secret_access_key=your_secret
+"""
+load_dotenv()
 
 # Load configuration from config.json
-with open('config\config.json', 'r') as config_file:
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# Path to config.json
+config_path = os.path.join(dir_path, 'config.json')
+
+# Load configuration from config.json
+with open(config_path, 'r') as config_file:
     config = json.load(config_file)
 
 AWS_CONFIG = config['aws']
@@ -15,8 +30,8 @@ STATISTICS = MONITORING_CONFIG['statistics']
 
 # Initialize a session using Boto3 with values from the config file
 session = boto3.Session(
-    aws_access_key_id=AWS_CONFIG['access_key_id'],
-    aws_secret_access_key=AWS_CONFIG['secret_access_key'],
+    aws_access_key_id=os.getenv('access_key_id'),
+    aws_secret_access_key=os.getenv('secret_access_key'),
     region_name=AWS_CONFIG['region_name']
 )
 
